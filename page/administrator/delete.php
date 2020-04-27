fbaee0bcb081d6712d281c68c056e6b85b70aa10
Swifter-n <54946777+Swifter-n@users.noreply.github.com>
2020-04-27 21:09:48 +0700

    Delete delete.php
    delete

--- page/administrator/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-if(isset($_GET['administrator-delete'])){
-	$id = $_GET['administrator-delete'];
-	
-	mysqli_query($conn, "DELETE FROM admin WHERE id = '$id'");
-	header("location: index.php?administrator");
-}
-?>
