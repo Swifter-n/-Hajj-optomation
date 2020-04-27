73adac6b13a69c3a7a1598b9fea5ab1a30b5f2dd
Swifter-n <54946777+Swifter-n@users.noreply.github.com>
2020-04-27 21:00:12 +0700

    Add files via upload
    add

---
+++ include/head.php
@@ -0,0 +1,14 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Dashboard Haji</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link href="font-awesome/css/font-awesome.css" rel="stylesheet" />
+    <link href="css/plugins/morris/morris-0.4.3.min.css" rel="stylesheet" />
+    <link href="css/plugins/timeline/timeline.css" rel="stylesheet" />
+    <link href="css/sb-admin.css" rel="stylesheet" />
+    <link href="css/custome.css" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="css/dist/sweetalert.css">
+    <link href="https://fonts.googleapis.com/css?family=Titillium+Web" rel="stylesheet">
+    <script type="text/javascript" src="css/dist/sweetalert.min.js"></script>
+</head>
---
+++ include/header.php
@@ -0,0 +1,114 @@
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation" style="margin-bottom: 0">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="index.php?home">Administrator</a>
+    </div>
+    <ul class="nav navbar-top-links navbar-right" >
+        <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
+            </a>
+            <ul class="dropdown-menu dropdown-user">
+                <li><a href="index.php?administrator"><i class="fa fa-user fa-fw"></i> Administrator</a></li>
+                <li class="divider"></li>
+                <li><a href="logout.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+    <div class="navbar-default navbar-static-side" role="navigation" >
+        <div class="sidebar-collapse">
+            <ul class="nav" id="side-menu">
+                <li>
+                    <a href="#"><i class="fa fa-dashboard fa-fw"></i>Optimization of Hajj quota<span class="fa arrow"></span></a>
+                    <ul class="nav nav-second-level" id="topFixedNavbar1">
+                        <li><a href="index.php?lapjkt">Data Haji Kuota Jakarta</a></li>
+                        <li><a href="index.php?lapbali">Data Haji Kuota Bali</a></li>
+                        <li><a href="index.php?lapaceh">Data Haji Kuota Aceh</a></li>
+                        <li><a href="index.php?lapbangka">Data Haji Kuota Bangka Belitung</a></li>
+                        <li><a href="index.php?lapbengkulu">Data Haji Kuota Bengkulu</a></li>
+                        <li><a href="index.php?lapjambi">Data Haji Kuota Jambi</a></li>
+                        <li><a href="index.php?laplampung">Data Haji Kuota Lampung</a></li>
+                        <li><a href="index.php?lapriau">Data Haji Kuota Riau</a></li>
+                        <li><a href="index.php?lapkepulauanriau">Data Haji Kuota Kepulauan Riau</a></li>
+                        <li><a href="index.php?lapjogya">Data Haji Kuota D.I Yogyakarta</a></li>
+                        <li><a href="index.php?lapgorontalo">Data Haji Kuota Gorontalo</a></li>
+                        <li><a href="index.php?lapbanten">Data Haji Kuota Banten</a></li>
+                        <li><a href="index.php?lapmaluku">Data Haji Kuota Maluku</a></li>
+                        <li><a href="index.php?lapmalukuutara">Data Haji Kuota Maluku Utara</a></li>
+                        <li><a href="index.php?lappapua">Data Haji Kuota Papua</a></li>
+                        <li><a href="index.php?lappapuabarat">Data Haji Kuota Papua Barat</a></li>
+                        <li><a href="index.php?lapjawabarat">Data Haji Kuota Jawa Barat</a></li>
+                        <li><a href="index.php?lapjawatimur">Data Haji Kuota Jawa Timur</a></li>
+                        <li><a href="index.php?lapjawatengah">Data Haji Kuota Jawa Tengah</a></li>
+                        <li><a href="index.php?lapkalimatanbarat">Data Haji Kuota Kalimantan Barat</a></li>
+                        <li><a href="index.php?lapkalimatanselatan">Data Haji Kuota Kalimantan Selatan</a></li>
+                        <li><a href="index.php?lapkalimatantimur">Data Haji Kuota Kalimantan Timur</a></li>
+                        <li><a href="index.php?lapkalimatantengah">Data Haji Kuota Kalimantan Tengah</a></li>
+                        <li><a href="index.php?lapnusatenggarabarat">Data Haji Kuota Nusatenggara Barat</a></li>
+                        <li><a href="index.php?lapnusatenggaratimur">Data Haji Kuota Nusatenggara Timur</a></li>
+                        <li><a href="index.php?lapsulewesibarat">Data Haji Kuota Sulawesi Barat</a></li>
+                        <li><a href="index.php?lapsulewesiselatan">Data Haji Kuota Sulawesi Selatan</a></li>
+                        <li><a href="index.php?lapsulewesitengah">Data Haji Kuota Sulawesi Tengah</a></li>
+                        <li><a href="index.php?lapsulewesitenggara">Data Haji Kuota Sulawesi Tenggara</a></li>
+                        <li><a href="index.php?lapsulewesiutara">Data Haji Kuota Sulawesi Utara</a></li>
+                        <li><a href="index.php?lapsumatrabarat">Data Haji Kuota Sumatera Barat</a></li>
+                        <li><a href="index.php?lapsumatraselatan">Data Haji Kuota Sumatera Selatan</a></li>
+                        <li><a href="index.php?lapsumatrautara">Data Haji Kuota Sumatera Utara</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#"><i class="fa fa-pencil-square-o fa-fw"></i>Region<span class="fa arrow"></span></a>
+                    <ul class="nav nav-second-level" id="topFixedNavbar1">
+                        <li><a href="index.php?jakarta">D.K.I Jakarta</a></li>
+                        <li><a href="index.php?jogya">Yogyakarta</a></li>
+                        <li><a href="index.php?bali">Bali</a></li>
+                        <li><a href="index.php?aceh">Aceh</a></li>
+                        <li><a href="index.php?banten">Banten</a></li>
+                        <li><a href="index.php?bengkulu">Bengkulu</a></li>
+                        <li><a href="index.php?riau">Riau</a></li>
+                        <li><a href="index.php?jambi">Jambi</a></li>
+                        <li><a href="index.php?lampung">Lampung</a></li>
+                        <li><a href="index.php?maluku">Maluku</a></li>
+                        <li><a href="index.php?maluku_utara">Maluku Utara</a></li>
+                        <li><a href="index.php?papua">Papua</a></li>
+                        <li><a href="index.php?papua_barat">Papua Barat</a></li>
+                        <li><a href="index.php?nusatenggara_barat">Nusatenggara Barat</a></li>
+                        <li><a href="index.php?nusatenggara_timur">Nusatenggara Timur</a></li>
+                        <li><a href="index.php?bangka">Bangka Belitung</a></li>
+                        <li><a href="index.php?gorontalo">Gorontalo</a></li>
+                        <li><a href="index.php?kalimatan_barat">Kalimantan Barat</a></li>
+                        <li><a href="index.php?kalimatan_timur">Kalimantan Timur</a></li>
+                        <li><a href="index.php?kalimatan_selatan">Kalimantan Selatan</a></li>
+                        <li><a href="index.php?kalimatan_tengah">Kalimantan Tengah</a></li>
+                        <li><a href="index.php?kepulauan_riau">Kepulauan Riau</a></li>
+                        <li><a href="index.php?sulewesi_barat">Sulewesi Barat</a></li>
+                        <li><a href="index.php?sulewesi_selatan">Sulewesi Selatan</a></li>
+                        <li><a href="index.php?sulewesi_tenggara">Sulewesi Tenggara</a></li>
+                        <li><a href="index.php?sulewesi_utara">Sulewesi Utara</a></li>
+                        <li><a href="index.php?sulewesi_tengah">Sulewesi Tengah</a></li>
+                        <li><a href="index.php?sumatra_barat">Sumatra Barat</a></li>
+                        <li><a href="index.php?sumatra_selatan">Sumatra Selatan</a></li>
+                        <li><a href="index.php?sumatra_utara">Sumatra Utara</a></li>
+                        <li><a href="index.php?jawa_barat">Jawa Barat</a></li>
+                        <li><a href="index.php?jawa_tengah">Jawa Tengah</a></li>
+                        <li><a href="index.php?jawa_timur">Jawa Timur</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#"><i class="fa fa-pencil-square-o fa-fw"></i>Hajj optimization report<span class="fa arrow"></span></a>
+                    <ul class="nav nav-second-level" id="topFixedNavbar1">
+                        <li><a href="page/laporan/laporan.php" target="_blank">Laporan Optimasi Haji</a>
+                        </li>
+                        <li><a href="index.php?table">Laporan Tabel Optimasi Haji</a>   
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
